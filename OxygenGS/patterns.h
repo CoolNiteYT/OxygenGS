2a1ed7430b5a76e24afb69a0126dfaf94f97e166
CoolNite <73061121+CoolNiteYT@users.noreply.github.com>
2022-08-17 12:35:06 +0200

    Added base for the gs
    - Added Oxygen GS solution
    - Added dllmain.cpp (Creates a thread for the main function , main function does nothing atm)
    - Added patterns.h (Only GObjects rn , find pattern function is empty)
    - Added already compiled DLL

---
+++ OxygenGS/patterns.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+#include <minwindef.h>
+namespace FNPatterns {
+	std::string GObjects = "";
+}
+void FindPattern(std::string pattern) {
+	//TODO : The whole function lol
+	//I have no idea how to do this rn lol
+}
